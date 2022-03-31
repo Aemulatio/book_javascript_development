17365cc86ad1c18acb8b5b623bae74c739798548
Stepan S <dumn2011@gmail.com>
2022-03-31 16:21:18 +0900

    апи на графКЛ

--- api/src/index.js
+++ api/src/index.js
@@ -5,19 +5,57 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 4000;
 
+
+const notes = [
+    {id: '1', content: "This is a note", author: "Adam Scott"},
+    {id: '2', content: "This is another note", author: "Scot Scott"},
+    {id: '3', content: "This is a third note", author: "S S"},
+]
+
+
 const typeDefs = gql`
-    type Query{
-        hello: String
-    }`;
+    type Query {
+        hello: String!
+        notes: [Note!]!
+        note(id: ID!): Note!
+    }
+
+    type Note {
+        id: ID!
+        content: String!
+        author: String!
+    }
+
+    type Mutation {
+        newNote(content: String!): Note!
+    }
+
+`;
 
 const resolvers = {
     Query: {
-        hello: () => "Hello world!"
+        hello: () => "Hello world!",
+        notes: () => notes,
+        note: (parent, args) => {
+            return notes.find(note => note.id === args.id);
+        },
+    },
+    Mutation: {
+        newNote: (parent, args) => {
+            let noteValue = {
+                id: String(notes.length + 1),
+                content: args.content,
+                author: "Stepan S",
+            };
+            notes.push(noteValue)
+            return noteValue
+        }
+
     }
 }
 
 const server = new ApolloServer({typeDefs, resolvers})
-server.applyMiddleware({app, path:"/api"})
+server.applyMiddleware({app, path: "/api"})
 
 
 app.get('/', (req, res) => res.send('Hello'));
