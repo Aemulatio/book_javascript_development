db5f10dd281dff83afc826fbee49232cbf8737ef
Stepan S <dumn2011@gmail.com>
2022-03-31 14:26:22 +0900

    apollo server

--- api/src/index.js
+++ api/src/index.js
@@ -1,8 +1,24 @@
 // index.js
 // This is the main entry point of our application
-const express = require('express')
+const {ApolloServer, gql} = require('apollo-server-express');
+const express = require('express');
 const app = express();
-const port = process.env.PORT || 4000
+const port = process.env.PORT || 4000;
 
-app.get("/", (req, res)=> res.send("Hello"))
-app.listen(port, ()=>console.log(`http://localhost:${port}`));
+const typeDefs = gql`
+    type Query{
+        hello: String
+    }`;
+
+const resolvers = {
+    Query: {
+        hello: () => "Hello world!"
+    }
+}
+
+const server = new ApolloServer({typeDefs, resolvers})
+server.applyMiddleware({app, path:"/api"})
+
+
+app.get('/', (req, res) => res.send('Hello'));
+app.listen(port, () => console.log(`http://localhost:${port}${server.graphqlPath}`));
