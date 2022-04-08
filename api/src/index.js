ed1d78d336856443d590e4abc146627a81cc49b4
Stepan S <dumn2011@gmail.com>
2022-04-08 13:41:10 +0900

    обновил настройки аполо

--- api/src/index.js
+++ api/src/index.js
@@ -5,6 +5,7 @@ const {ApolloServer} = require('apollo-server-express');
 const typeDefs = require("./schema")
 const express = require('express');
 const app = express();
+const jwt = require('jsonwebtoken')
 
 const port = process.env.PORT || 4000;
 const DB_HOST = process.env.DB_HOST;
@@ -14,11 +15,25 @@ db.connect(DB_HOST);
 const models = require("./models")
 const resolvers = require("./resolvers")
 
+const getUser = (token) => {
+  if (token){
+      try {
+          return jwt.verify(token, process.env.JWT_SECRET)
+      } catch (err) {
+          new Error("Session invalid")
+      }
+  }
+}
+
 const server = new ApolloServer({
     typeDefs,
     resolvers,
-    context: () => {
-        return {models};
+    context: ({req}) => {
+        const token = req.headers.authorization;
+        const user = getUser(token);
+        console.log(user)
+
+        return {models, user};
     }
 })
 server.applyMiddleware({app, path: "/api"})
