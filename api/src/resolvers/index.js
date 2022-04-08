7f6b85db9919aca07eee08068e375f583aaf7001
Stepan S <dumn2011@gmail.com>
2022-04-08 11:10:02 +0900

    поддержка дата тайм

--- api/src/resolvers/index.js
+++ api/src/resolvers/index.js
@@ -1,6 +1,9 @@
 const Query = require("./query")
 const Mutation = require("./mutation")
+const {GraphQLDateTime} = require('graphql-iso-date')
 
 module.exports = {
-    Query, Mutation
+    Query,
+    Mutation,
+    DateTime: GraphQLDateTime
 };
