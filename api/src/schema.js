baa877df85b6da25e12456110edeb99b25934aa4
Stepan S <dumn2011@gmail.com>
2022-04-08 12:53:02 +0900

    регистрация и авторизация пользователя

--- api/src/schema.js
+++ api/src/schema.js
@@ -1,7 +1,7 @@
 const {gql} = require("apollo-server-express");
 module.exports = gql`
     scalar DateTime
-    
+
     type Query {
         notes: [Note!]!
         note(id: ID!): Note!
@@ -10,16 +10,26 @@ module.exports = gql`
     type Note {
         id: ID!
         content: String!
-        author: String!
+        author: User!
         createdAt: DateTime!
         updatedAt: DateTime!
+    }
+
+    type User{
+        id: ID!
+        username: String!
+        email: String!
+        avatar: String
+        notes: [Note!]!
     }
 
     type Mutation {
         newNote(content: String!): Note!
         updateNote(id: ID!, content: String!): Note!
         deleteNote(id: ID!): Boolean!
-        
+        signUp(username: String!, email:String!, password: String!): String!
+        signIn(username: String, email:String, password: String!): String!
+
     }
 
 `;
