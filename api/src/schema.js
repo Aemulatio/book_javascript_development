896ef60b9e2f81caac095a5cd6ae94e82f07b030
Stepan S <dumn2011@gmail.com>
2022-04-15 10:26:38 +0900

    пагинация

--- api/src/schema.js
+++ api/src/schema.js
@@ -8,6 +8,8 @@ module.exports = gql`
         user(username: String!): User!
         users: [User!]!
         me: User!
+        noteFeed(cursor: String): NoteFeed
+        
     }
 
     type Note {
@@ -29,6 +31,12 @@ module.exports = gql`
         favorites: [Note!]!
     }
 
+    type NoteFeed{
+        notes: [Note]! 
+        cursor: String!
+        hasNextPage: Boolean!
+    }
+    
     type Mutation {
         newNote(content: String!): Note!
         updateNote(id: ID!, content: String!): Note!
