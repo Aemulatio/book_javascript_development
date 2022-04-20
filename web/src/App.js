298c8c7c0a998e527c9fde282344be9e34772051
Stiiipan <dumn2011@gmail.com>
2022-04-20 16:59:52 +0900

    check

--- web/src/App.js
+++ web/src/App.js
@@ -4,6 +4,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import './App.css';
+
 const App = () => {
     return (
         <div>
@@ -13,4 +15,4 @@ const App = () => {
     );
 };
 
-ReactDOM.render(<App/>, document.querySelector("#root"))
+export default App;
