a7df813456de8d2e618bef364923291c12adbd69
Stiiipan <dumn2011@gmail.com>
2022-04-21 11:35:39 +0900

    Процесс в пакете, Рендер 18 реакта

--- web/src/App.js
+++ web/src/App.js
@@ -2,18 +2,27 @@
 // This is the main entry point of our application
 
 import React from "react";
-import ReactDOM from "react-dom";
+import {createRoot} from "react-dom/client";
 
 import Pages from "./pages";
 import GlobalStyle from "./components/GlobalStyle";
 
+import {ApolloClient, ApolloProvider, InMemoryCache} from "@apollo/client";
+
+const uri = process.env.API_URI;
+const cache = new InMemoryCache();
+
+const client = new ApolloClient({
+    uri, cache, connectToDevTools: true
+})
+
 const App = () => {
     return (
-        <div>
+        <ApolloProvider client={client}>
             <GlobalStyle/>
             <Pages/>
-        </div>
+        </ApolloProvider>
     );
 };
 
-ReactDOM.render(<App/>, document.querySelector("#root"))
+createRoot(document.querySelector("#root")).render(<App/>)
