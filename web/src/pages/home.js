6151d97929bc901c9e28caf86424a9c62b6225fa
Stiiipan <dumn2011@gmail.com>
2022-04-21 13:00:44 +0900

    запрос на получение данных из бд

--- web/src/pages/home.js
+++ web/src/pages/home.js
@@ -1,15 +1,48 @@
 import React from "react";
+import {useQuery, gql} from "@apollo/client";
 
 import Header from "../components/Header";
 import Navigation from "../components/Navigation";
 import Button from "../components/Button";
 
+
+const GET_NOTES = gql`
+  query noteFeed($cursor: String) {
+    noteFeed(cursor: $cursor) {
+      cursor
+      hasNextPage
+      notes {
+        id
+        createdAt
+        content
+        favoriteCount
+        author {
+          username
+          id
+          avatar
+        }
+      }
+    }
+  }
+`;
+
 const Home = () => {
-  return(
-    <div>
-      <p>Home page</p>
-      <Button>Click me</Button>
-    </div>
-  )
+    const {data, loading, error, fetchMore} = useQuery(GET_NOTES);
+
+    if (loading) return (
+        <>
+            <p>Loading...</p>
+            {console.log(error)}
+        </>);
+
+    if (error) return <p>Error!</p>;
+
+    return (
+        <div>
+            {console.log(data)}
+            <p>Data loaded!</p>
+            <Button>Click me</Button>
+        </div>
+    )
 }
 export default Home
