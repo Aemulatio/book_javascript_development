450bb6f1836904cf955ab879106e24c129880683
Stiiipan <dumn2011@gmail.com>
2022-04-21 09:45:36 +0900

    Глобальные стили

--- web/src/App.js
+++ web/src/App.js
@@ -3,12 +3,14 @@
 
 import React from "react";
 import ReactDOM from "react-dom";
-import Pages from "./pages";
 
+import Pages from "./pages";
+import GlobalStyle from "./components/GlobalStyle";
 
 const App = () => {
     return (
         <div>
+            <GlobalStyle/>
             <Pages/>
         </div>
     );
