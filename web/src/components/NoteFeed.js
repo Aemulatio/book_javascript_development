5e84c92bccbe6425e646bca3e140323217b90295
Stepan S <dumn2011@gmail.com>
2022-04-27 14:07:41 +0900

    стилизован список заметок

--- web/src/components/NoteFeed.js
+++ web/src/components/NoteFeed.js
@@ -1,16 +1,24 @@
 import React from "react";
 import Note from "./Note";
+import styled from "styled-components";
 
-const NoteFeed = ({notes}) => {
-    return (
-        <div>
-            {notes.map(note => (
-                <div key={note.id}>
-                    <Note note={note}/>
-                </div>
-            ))}
-        </div>
-    )
-}
+const NoteWrapper = styled.div`
+  max-width: 800px;
+  margin: 0 auto 2em;
+  padding-bottom: 2em;
+  border-bottom: 1px solid #f5f4f0;
+`;
+
+const NoteFeed = ({ notes }) => {
+  return (
+    <div>
+      {notes.map(note => (
+        <NoteWrapper key={note.id}>
+          <Note note={note} />
+        </NoteWrapper>
+      ))}
+    </div>
+  );
+};
 
 export default NoteFeed;
