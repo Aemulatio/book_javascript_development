9b4738665c5deda37639b761ff4dc84d25607620
Stepan S <dumn2011@gmail.com>
2022-04-27 14:03:09 +0900

    Отформатированы заметки

--- web/src/components/Note.js
+++ web/src/components/Note.js
@@ -1,14 +1,52 @@
 import React from "react";
 import ReactMarkdown from "react-markdown";
 
-const Note = ({note}) => {
-    return (
-        <article key={note.id}>
-            <img src={note.author.avatar} alt={`${note.author.username} avatar`} height="50px"/>{" "}
-            {note.author.username} {note.createdAt} {note.favoriteCount}{' '}
-            <ReactMarkdown source={note.content}/>
-        </article>
-    )
-}
+import { format, parseISO } from "date-fns";
+import styled from "styled-components";
+
+
+const StyledNote = styled.article`
+  max-width: 800px;
+  margin: 0 auto;
+`;
+
+const MetaData = styled.div`
+  @media ( min-width: 500px) {
+    display: flex;
+    align-items: flex-start;
+  }
+`;
+
+const MetaInfo = styled.div`
+  padding-right: 1em;
+`;
+
+const UserActions = styled.div`
+  margin-left: auto;
+`;
+
+const Note = ({ note }) => {
+  return (
+    <StyledNote>
+      <MetaData>
+        <MetaInfo>
+          <img src={note.author.avatar}
+               alt={`${note.author.username} avatar`}
+               height="50px"
+          />
+        </MetaInfo>
+        <MetaInfo>
+          <em>by</em> {note.author.username} <br />
+          {format(parseISO(note.createdAt), "MMM Do yyyy")}
+        </MetaInfo>
+        <UserActions>
+          <em>Favorites:</em> {note.favoriteCount}
+          <ReactMarkdown source={note.content} />
+        </UserActions>
+      </MetaData>
+      <ReactMarkdown source={note.content} />
+    </StyledNote>
+)
+};
 
 export default Note;
