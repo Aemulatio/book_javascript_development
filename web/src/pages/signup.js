7c90efcb437fb40612f9525c73be5e50b00d61a7
Stiiipan <dumn2011@gmail.com>
2022-04-29 14:24:59 +0900

    верстка формы регистрации и стили

--- web/src/pages/signup.js
+++ web/src/pages/signup.js
@@ -1,4 +1,27 @@
 import React, {useEffect} from "react";
+import styled from 'styled-components'
+
+import Button from "../components/Button";
+
+const Wrapper = styled.div`
+  border: 1px solid #f5f4f0;
+  max-width: 500px;
+  padding: 1em;
+  margin: 0 auto;
+`;
+
+const Form = styled.form`
+  label, input {
+    display: block;
+    line-height: 2em;
+  }
+
+  input {
+    width: 100%;
+    margin-bottom: 1em;
+  }
+`;
+
 
 const SignUp = (props) => {
     useEffect(() => {
@@ -6,9 +29,20 @@ const SignUp = (props) => {
     });
 
     return (
-        <>
-            <p>Sign up</p>
-        </>
+        <Wrapper>
+            <h2>Sign Up</h2>
+            <Form>
+                <label htmlFor="username">Username:</label>
+                <input type="text" required id="username" name="username" placeholder="username"/>
+
+                <label htmlFor="email">Email:</label>
+                <input type="email" required id="email" name="email" placeholder="Email"/>
+
+                <label htmlFor="password">Password:</label>
+                <input type="password" required id="password" name="password" placeholder="Password"/>
+                <Button type='submit'>Submit</Button>
+            </Form>
+        </Wrapper>
     )
 }
 
