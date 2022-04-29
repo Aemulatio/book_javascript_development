fc23c7209457d495388f72914f1879d3e2c2b2d4
Stiiipan <dumn2011@gmail.com>
2022-04-29 14:11:46 +0900

    Страница для регистрации

--- web/src/pages/index.js
+++ web/src/pages/index.js
@@ -11,6 +11,7 @@ import Home from "./home";
 import MyNotes from "./mynotes";
 import Favorites from "./favorites";
 import NotePage from "./note";
+import SignUp from "./signup";
 
 const Pages = () => {
   return (
@@ -21,6 +22,7 @@ const Pages = () => {
           <Route path="/mynotes" element={<MyNotes />} />
           <Route path="/favorites" element={<Favorites />} />
           <Route path="/note/:id" element={<NotePage />} />
+          <Route path="/signup" element={<SignUp />} />
         </Routes>
       </Layout>
     </Router>
---
+++ web/src/pages/signup.js
@@ -0,0 +1,15 @@
+import React, {useEffect} from "react";
+
+const SignUp = (props) => {
+    useEffect(() => {
+        document.title = "Sign Up - Notedly";
+    });
+
+    return (
+        <>
+            <p>Sign up</p>
+        </>
+    )
+}
+
+export default SignUp
