9ac7fb8498095ff72a8bd26a4c927c67166742a3
Stepan S <dumn2011@gmail.com>
2022-05-03 13:25:37 +0900

    глобальная настройка

--- web/src/App.js
+++ web/src/App.js
@@ -2,27 +2,36 @@
 // This is the main entry point of our application
 
 import React from "react";
-import {createRoot} from "react-dom/client";
+import { createRoot } from "react-dom/client";
 
 import Pages from "./pages";
 import GlobalStyle from "./components/GlobalStyle";
 
-import {ApolloClient, ApolloProvider, InMemoryCache} from "@apollo/client";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 
 const uri = process.env.API_URI;
 const cache = new InMemoryCache();
 
 const client = new ApolloClient({
-    uri, cache, connectToDevTools: true
-})
+  link: authLink.concat(httpLink),
+  cache,
+  resolvers: {},
+  connectToDevTools: true
+});
 
-const App = () => {
-    return (
-        <ApolloProvider client={client}>
-            <GlobalStyle/>
-            <Pages/>
-        </ApolloProvider>
-    );
+const data = {
+  isLoggedIn: !!localStorage.getItem("token")
 };
 
-createRoot(document.querySelector("#root")).render(<App/>)
+cache.writeData({data})
+
+const App = () => {
+  return (
+    <ApolloProvider client={client}>
+      <GlobalStyle />
+      <Pages />
+    </ApolloProvider>
+  );
+};
+
+createRoot(document.querySelector("#root")).render(<App />);
