94e390ab8264a0b98df56f170269c57c532ec038
Stiiipan <dumn2011@gmail.com>
2022-05-06 16:29:27 +0900

    Теперь есть страница

--- web/src/pages/signin.js
+++ web/src/pages/signin.js
@@ -1,13 +1,36 @@
 import React, {useEffect} from 'react'
+import {useMutation, useApolloClient, gql} from "@apollo/client";
+
+import {useNavigate} from "react-router-dom";
+import UserForm from "../components/UserForm";
+
+
+const SIGNIN_USER = gql`
+    mutation signIn($email: String!, $password: String!){
+        signUp(email: $email, password: $password)
+    }
+`;
+
 
 const SignIn = () => {
     useEffect(() => {
         document.title = "Sign In - Notedly";
     });
 
+    const navigate = useNavigate();
+    const client = useApolloClient();
+    const [signIn, {loading, error}] = useMutation(SIGNIN_USER, {
+        onCompleted: data => {
+            localStorage.setItem("token", data.signIn);
+            navigate("/");
+        }
+    })
+
     return (
         <>
-            <p>Sign up page</p>
+            <UserForm action={signIn} formType="signIn"/>
+            {loading && <p>Loading...</p>}
+            {error && <p>Error signing in!</p>}
         </>
     );
 }
