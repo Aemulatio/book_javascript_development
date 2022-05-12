b16f7204d1371d6caf041c16aca62f54190b2b6f
Stepan S <dumn2011@gmail.com>
2022-05-12 15:33:37 +0900

    render

--- web/src/pages/new.js
+++ web/src/pages/new.js
@@ -1,11 +1,12 @@
 import React, { useEffect } from "react";
 import { useMutation, gql } from "@apollo/client";
+import NoteForm from "../components/NoteForm";
 
 const NewNote = props => {
-  useEffect(()=>{
-    document.title = "New Note - Notedly"
+  useEffect(() => {
+    document.title = "New Note - Notedly";
   });
-  return <div>New note</div>
+  return <NoteForm />;
 };
 
 export default NewNote;
