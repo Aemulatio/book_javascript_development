55b65b0e5aa5f7140eb8a199ba461146308cf916
Stepan S <dumn2011@gmail.com>
2022-05-12 16:02:21 +0900

    сделал чтото....
    забью на это пока что, аполло что-то слишком непонятный

--- web/src/pages/new.js
+++ web/src/pages/new.js
@@ -1,12 +1,46 @@
 import React, { useEffect } from "react";
 import { useMutation, gql } from "@apollo/client";
 import NoteForm from "../components/NoteForm";
+import { useNavigate } from "react-router-dom";
+
+const NEW_NOTE = gql`
+    mutation newNote($content: String!){
+        newNote (content: $content){
+            id
+            content
+            createdAt
+            favoriteCount
+            favoritedBy {
+                id
+                username
+            }
+            author {
+                id
+                username
+            }
+        }
+    }
+`;
 
 const NewNote = props => {
   useEffect(() => {
     document.title = "New Note - Notedly";
   });
-  return <NoteForm />;
+
+  const navigate = useNavigate();
+  const [data, { loading, error }] = useMutation(NEW_NOTE, {
+    onCompleted: data => {
+      navigate(`note/${data.newNote.id}`);
+    }
+  });
+
+  return (
+    <>
+      {loading && <p>Loading....</p>}
+      {error && <p>Error saving the note</p>}
+      <NoteForm action={data} />
+    </>
+  );
 };
 
 export default NewNote;
